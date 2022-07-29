5dca2cdb51aa9e68e3542ab4e6761e048c4931f7
Hans M. Rupp <hansrupp@aol.com>
2022-07-29 11:01:26 +0200

    first svm model

--- project_limma.R
+++ project_limma.R
@@ -20,18 +20,13 @@ labels <- read_csv("actual.csv")
 train_data <- train_data %>% select(-contains("call"))
 #View(train_data)
 
-#In train data the columns are not sorted by the patient number
-train_data_desc <- train_data[, 1:2]
-train_data_data <- train_data[, 3:ncol(train_data)]
-train_data_data <- train_data_data[, order(as.numeric(names(train_data_data)))]
-#View(train_data_data)
 
-train_data <- cbind(train_data_desc, train_data_data)
 
 #View(train_data)
 ncol(train_data)
 test_data <- test_data %>% select(-contains("call"))
 ncol(test_data)
+#View(test_data)
 
 #Check for missing values
 nas <- function(x) ( any(is.na(x)) )
@@ -44,9 +39,17 @@ print(test_data %>% summarise_all(nas), width = Inf)
 all_data <- train_data %>% left_join(test_data)
 ncol(all_data)
 nrow(all_data)
-View(all_data)
-str(all_data)
+#View(all_data)
+#str(all_data)
 #View(labels)
+
+#Sort patient by patient numbers
+all_data_desc <- all_data[, 1:2]
+all_data_data <- all_data[, 3:ncol(all_data)]
+all_data_data <- all_data_data[, order(as.numeric(names(all_data_data)))]
+
+all_data <- cbind(all_data_desc, all_data_data)
+View(all_data)
 
 #features
 f <- all_data[, 1:2]
@@ -84,4 +87,44 @@ results <- decideTests(fit[, "cancerAML"])
 summary(results)
 
 #List of differentially expressed genes
-topTable(fit, n=70, adjust="fdr")
+top_DE_genes <- topTable(fit, n=70, adjust="fdr")
+
+str(all_data)
+top_data <- all_data %>% filter(`Gene Accession Number` %in% top_DE_genes$Gene.Accession.Number)
+
+View(top_data)
+
+#numerical matrix
+x_m <- top_data[, 3:ncol(top_data)]
+#View(x_m)
+
+
+#Transform to make tidy: observations = patients in rows, genes in columns
+x_m <- t(x_m)
+ncol(x_m) #70 top DE genes
+nrow(x_m) ##72 patients
+
+colnames(x_m) <- top_data$`Gene Accession Number`
+View(x_m)
+
+#Normalize
+x_m_norm <- scale(x_m)
+#x_m_norm <- sweep(x_m, 2, colMeans(x_m))
+#x_m_norm <- sweep(x_m_norm, 2, colSds(x_m_norm), "/")
+
+
+#Create Train- and Testdata
+indexTrain <- createDataPartition(labels$cancer, p=0.66, list = F)
+norm_train_data <- x_m_norm[indexTrain, ]
+norm_test_data <- x_m_norm[-indexTrain, ]
+train_label <- labels[indexTrain, ]
+test_label <- labels[-indexTrain, ]
+str(norm_train_data)
+nrow(norm_train_data)
+nrow(train_label)
+
+#SVN
+svmlinear1 <- train(x = norm_train_data, y= train_label$cancer, method = "svmLinear")
+svmlinear1
+
+`
